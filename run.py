399e2356bd9ed965cbbfc3da3687ca4a4a07fe46
Ville Laitila <ville.laitila@softagram.com>
2018-09-13 09:41:49 +0300

    dummy change for testing PR analysis to re-analyse

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 # -*- encoding: utf-8 -*-
 # vim: set et sw=4 ts=4 sts=4 ff=unix fenc=utf8:
 # Author: Binux<roy@binux.me>
-#         http://binux.me
+#         http://binux.me 
 # Created on 2014-11-24 23:11:49
 
 from pyspider.run import main
